34d5829cbc71db34fb80058ed11a71ec414c0a85
saufth <saufth@gmail.com>
2024-08-21 00:39:06 -0600

    update font sans & hero font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,9 +1,16 @@
-import { Roboto as RobotoFont } from 'next/font/google'
 import localFont from 'next/font/local'
 
-export const fontSans = RobotoFont({
-  weight: ['100', '300', '400', '500', '700', '900'],
-  subsets: ['latin'],
+export const fontSans = localFont({
+  src: [
+    {
+      path: '../../public/fonts/sans-regular.woff2',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/sans-medium.woff2',
+      weight: '500'
+    }
+  ],
   display: 'swap',
   variable: '--font-sans'
 })
