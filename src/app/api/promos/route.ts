1ad4957957ff43f5254dd6dc24c2c5342f14c299
saufth <saufth@gmail.com>
2024-09-21 11:59:58 -0600

    fix database import on promos route

--- src/app/api/promos/route.ts
+++ src/app/api/promos/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server'
-import { pool } from '@/lib/database'
+import { db } from '@/lib/database'
 import { type RowDataPacket } from 'mysql2'
 
 export async function GET () {
-  const result: RowDataPacket = await pool.query('select CURRENT_TIMESTAMP')
+  const result: RowDataPacket = await db.query('select CURRENT_TIMESTAMP')
   return NextResponse.json({ message: result[0].CURRENT_TIMESTAMP })
 }
 
