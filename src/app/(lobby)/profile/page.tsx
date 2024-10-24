196dc7ce2b2f98dc076de20626cfb105c5aeb0ad
saufth <saufth@gmail.com>
2024-10-23 19:16:48 -0600

    testing profile page9

--- src/app/(lobby)/profile/page.tsx
+++ src/app/(lobby)/profile/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 import QRCodeImage from '@/components/qr-code'
 import { getSession } from '@/lib/actions/session'
 import { getUserProfile } from '@/lib/actions/users'
-import { roles, userStatus } from '@/lib/constants'
+import { redirects, roles, userStatus } from '@/lib/constants'
 import { absoluteUrl } from '@/lib/utils'
 import { GENRE } from '@/config/app'
 import { signout } from '@/lib/actions/auth'
@@ -11,24 +11,24 @@ export default async function ProfilePage () {
   const role = roles.user
   const session = await getSession(role)
 
-  if (!session.data) {
+  if (session.error) {
     await signout(role)
-    redirect('/#home1')
+    redirect(redirects.toSignin)
   }
 
-  const userId = { id: String(session.data.id) }
+  const userId = { id: String(session.data?.id) }
   const userProfile = await getUserProfile(userId)
 
   if (!userProfile.data) {
     if (userProfile.error) {
       if (userProfile.error === userStatus.unverified) {
-        redirect('/#home2')
+        redirect(redirects.toSignin)
       }
       await signout(role)
-      redirect('/#home3')
+      redirect(redirects.afterLogout)
     }
     await signout(role)
-    redirect('/#home4')
+    redirect(redirects.toSignin)
   }
 
   const profile = userProfile.data
