e86316a7efafade00a6b593143843050aa787398
saufth <saufth@gmail.com>
2024-10-23 19:21:50 -0600

    testing profile page9

--- src/app/(lobby)/profile/page.tsx
+++ src/app/(lobby)/profile/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 import QRCodeImage from '@/components/qr-code'
 import { getSession } from '@/lib/actions/session'
 import { getUserProfile } from '@/lib/actions/users'
-import { redirects, roles, userStatus } from '@/lib/constants'
+import { redirects, roles } from '@/lib/constants'
 import { absoluteUrl } from '@/lib/utils'
 import { GENRE } from '@/config/app'
 import { signout } from '@/lib/actions/auth'
@@ -19,17 +19,17 @@ export default async function ProfilePage () {
   const userId = { id: String(session.data?.id) }
   const userProfile = await getUserProfile(userId)
 
-  if (!userProfile.data) {
-    if (userProfile.error) {
-      if (userProfile.error === userStatus.unverified) {
-        redirect(redirects.toSignin)
-      }
-      await signout(role)
-      redirect(redirects.afterLogout)
-    }
-    await signout(role)
-    redirect(redirects.toSignin)
-  }
+  // if (!userProfile.data) {
+  //   if (userProfile.error) {
+  //     if (userProfile.error === userStatus.unverified) {
+  //       redirect(redirects.toSignin)
+  //     }
+  //     await signout(role)
+  //     redirect(redirects.afterLogout)
+  //   }
+  //   await signout(role)
+  //   redirect(redirects.toSignin)
+  // }
 
   const profile = userProfile.data
 
@@ -37,16 +37,16 @@ export default async function ProfilePage () {
     <div>
       <div className='container py-spacing-6'>
         <div>
-          {`${profile.firstName} ${profile.lastName}`}
+          {`${profile?.firstName} ${profile?.lastName}`}
         </div>
         <div>
-          {profile.email}
+          {profile?.email}
         </div>
         <div>
-          {GENRE.find((genreItem) => String(profile.genreISO) === genreItem.iso)?.title}
+          {GENRE.find((genreItem) => String(profile?.genreISO) === genreItem.iso)?.title}
         </div>
         <div>
-          {String(profile.birthday)}
+          {String(profile?.birthday)}
         </div>
         <QRCodeImage src={absoluteUrl(`/profile/${userId}`)} />
       </div>
